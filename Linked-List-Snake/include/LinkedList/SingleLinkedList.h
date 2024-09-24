45d3ea9bb0f6295ba6cd32fd1d7f3d4c9fe5e98f
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-09-24 11:17:06 +0530

    updated single linked list

--- Linked-List-Snake/include/LinkedList/SingleLinkedList.h
+++ Linked-List-Snake/include/LinkedList/SingleLinkedList.h
@@ -3,15 +3,27 @@
 #include "LinkedList/Node.h"
 #include "Player/Direction.h"
 
+#include <SFML/System/Vector2.hpp>
+
 namespace LinkedList
 {
 	class SingleLinkedList
 	{
 	private:
 		Node* head_node;
+
 		float node_width;
 		float node_height;
+
 		sf::Vector2i default_position;
 		Player::Direction default_direction;
+
+		Node* createNode();
+
+	public:
+		SingleLinkedList();
+		~SingleLinkedList();
+		void initialize(float width, float height, sf::Vector2i position, Player::Direction direction);
+		void render();
 	};
 }
